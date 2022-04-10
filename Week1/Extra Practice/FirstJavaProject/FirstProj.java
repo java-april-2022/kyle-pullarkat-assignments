2cb7f7d58d26b3bc278aaba7dcb501dd0745b9d9
Kyle P <kpullarkat@yahoo.com>
2022-04-10 11:05:31 -0700

    firstproj

---
+++ Week1/Extra Practice/FirstJavaProject/FirstProj.java
@@ -0,0 +1,7 @@
+public class FirstProj {
+    public static void main(String[] args){
+        System.out.println("My name is Kyle Pullarkat");
+        System.out.println("I am 22 Years old");
+        System.out.println("My Hometown is Las Vegas, NV");
+    }
+}
